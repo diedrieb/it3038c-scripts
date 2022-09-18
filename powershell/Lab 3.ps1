9fb8a28b3870eda1e60959ed43647416af664956
diedrieb <86367334+diedrieb@users.noreply.github.com>
2022-09-18 18:40:27 -0400

    Add files via upload

---
+++ powershell/Lab 3.ps1
@@ -0,0 +1,27 @@
+﻿function GetIP {
+   (get-netipaddress).ipv4address | Select-String "192*"
+}
+
+function GetUser {
+    whoami
+} 
+
+function GetHostname {
+    hostname
+}
+
+function Version {
+    $PSVersionTable.PSVersion
+}
+
+function TodayDate {
+    Get-Date -Format "dddd MM/dd/yyyy"
+}
+
+$IP = GetIP
+$USER = GetUser
+$HOSTNAME = GetHostname
+$VERSION = Version 
+$DATE = TodayDate
+
+Write-Host("This machine's IP is $IP. User is $USER. Hostname is $HOSTNAME. Powershell Version $VERSION. Today's date is $DATE.")
